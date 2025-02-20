d7c044f152ed1064ead9bbd6a4a110d2ee589994
Wilson Bravo <bravoc.wilson@gmail.com>
2025-02-20 12:10:32 -0500

    feat: add swagger documentation

--- backend/src/modules/routines/routine.controller.ts
+++ backend/src/modules/routines/routine.controller.ts
@@ -10,26 +10,33 @@ import {
   UsePipes,
   ValidationPipe,
 } from "@nestjs/common";
+import { ApiOkResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
 
-import { CreateRoutineDto } from "./dto/dto";
+import { CreateRoutineDto, FindAllRoutinesDto } from "./dto/dto";
 import { RoutineService } from "./routine.service";
 
+@ApiTags("routines")
 @Controller("routines")
 export class RoutineController {
   constructor(private routineService: RoutineService) {}
 
   @Get("")
+  @ApiOperation({ summary: "Find all routines" })
+  @ApiOkResponse({ type: FindAllRoutinesDto, isArray: true })
   getUsers() {
     return this.routineService.findAll();
   }
 
   @Post("")
+  @ApiOperation({ summary: "Create new routine" })
+  @ApiOkResponse({ type: FindAllRoutinesDto, isArray: false })
   @UsePipes(new ValidationPipe({ whitelist: true }))
   create(@Body() data: CreateRoutineDto) {
     return this.routineService.create(data);
   }
 
   @Delete(":id")
+  @ApiOperation({ summary: "Delete routine by id" })
   @HttpCode(204)
   async delete(@Param("id") id: string) {
     if (!id) {
