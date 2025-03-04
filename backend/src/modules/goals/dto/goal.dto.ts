03f6a1eb6f0453460389c1c58e8ce46ed44d2a3f
Wilson Bravo <bravoc.wilson@gmail.com>
2025-03-03 22:37:30 -0500

    fix: update validations

--- backend/src/modules/goals/dto/goal.dto.ts
+++ backend/src/modules/goals/dto/goal.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsEnum, IsInt, IsUUID } from "class-validator";
+import { IsEnum, IsInt, IsUUID, Min } from "class-validator";
 
 import { Period } from "@/common/enums/enums";
 import { type Period as PeriodType } from "@/common/types/types";
@@ -15,6 +15,7 @@ class GoalDto {
 
   @ApiProperty()
   @IsInt()
+  @Min(0)
   value!: number;
 
   @ApiProperty()
