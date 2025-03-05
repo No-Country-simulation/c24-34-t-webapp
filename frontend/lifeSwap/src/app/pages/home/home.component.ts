684bed531d36cd0c78bfd0b8f932f889ae0dabb8
celes <celeste2325@gmail.com>
2025-03-05 01:18:20 -0500

    feat: #47 display multiple routines in home component

--- frontend/lifeSwap/src/app/pages/home/home.component.ts
+++ frontend/lifeSwap/src/app/pages/home/home.component.ts
@@ -1,18 +1,19 @@
 import { Component, Input } from '@angular/core';
-import { Activity } from '../../models/routine';
+import {Activity, Routine} from '../../models/routine';
 import { BtnDisplayList } from '../../components/btn-display-list/btn-display-list';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CommonModule, TitleCasePipe } from '@angular/common';
 import { CdkAccordionModule } from '@angular/cdk/accordion';
 import { get_icons } from '../../models/get_icons';
-import {ActivatedRoute, Router, RouterLink} from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { FilterComponent } from '../../components/filter/filter.component';
-import {UsersService} from '../../services/users.service';
 import {RequestStatus} from '../../models/request-status.model';
 import {Dialog} from '@angular/cdk/dialog';
 import {ModelMessagesComponent} from '../../components/model-messages/model-messages.component';
 import {FormErrorMessageComponent} from '../../components/form-error-message/form-error-message.component';
 import {Color_btn} from '../../models/color_btn';
+import {AuthService} from '../../services/auth.service';
+import {User} from '../../models/User';
 
 @Component({
   selector: 'app-home',
@@ -33,43 +34,42 @@ export class HomeComponent {
   //user activities are input when send from the filter.
   @Input() activities: Activity[] = [];
   general_icons = get_icons;
-  emailUser: string = '';
   status: RequestStatus = 'init';
   colorBtn = Color_btn;
+  userInformation:User =
+    {
+      id:'',
+      username:'',
+      email:'',
+      routines: [],
+      accessToken:'',
+    }
+  ;
 
   constructor(
-    private route: ActivatedRoute,
-    private usersService: UsersService,
     private dialog: Dialog,
     private router:Router,
+    private authService:AuthService
   ) {}
 
   ngOnInit() {
-    this.getRoutines();
+    this.getUserInformation();
   }
 
-  getRoutines() {
+  getUserInformation() {
     //set status to loading while waiting for the backend response
     this.status='loading';
     //open a dialog with a message about the pending status
     this.openDialog();
 
-    this.route.params.subscribe((params) => {
-      //email from login component
-      this.emailUser = params['email'];
-    });
-
-    this.usersService.gerUserByEmail(this.emailUser).subscribe({
+    this.authService.verifyToken().subscribe({
       next:(result)=> {
-        if (result.routines.length>0){
-          result.routines.forEach(routine => {
-            if (routine.activities.length >0){
-              this.activities = routine.activities;
-            }
-          })
-        }
+        this.status = 'success';
+        this.dialog.closeAll();
+        this.userInformation = result;
       },
       error:(err)=> {
+        this.dialog.closeAll();
         if (err.status === 404){
           //set status to notFound when a 404 error occurs
           this.status = "notFound"
@@ -94,6 +94,10 @@ export class HomeComponent {
     })
   }
 
+  showRoutineActivities(routine:Routine){
+    this.activities = routine.activities;
+    console.log('activities',routine.activities)
+  }
   //activities when the user use the filter
   setActivitiesFiltered(activities: Activity[]) {
     this.activities = activities;
